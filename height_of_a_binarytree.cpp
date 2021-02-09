50c5ada78e11a4066d5af5a1af765179da455b96
Amartya pandey <amartyapandey19@gmail.com>
2021-02-09 08:13:22 +0530

    Update height_of_a_binarytree.cpp

--- height_of_a_binarytree.cpp
+++ height_of_a_binarytree.cpp
@@ -1,39 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+class solultion{
 int height(Node* root){
     if(root==NULL){
         return 0;
     }
+    
     int hl=height(root->left);
     int h2=height(root->right);
     return (max(h1+h2)+1);
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 
