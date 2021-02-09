73290ac61927de6ea22337c43e2e19a3592286f6
Amartya pandey <amartyapandey19@gmail.com>
2021-02-09 08:01:05 +0530

    Update DiagonallyTraversebinarytree.cpp

--- DiagonallyTraversebinarytree.cpp
+++ DiagonallyTraversebinarytree.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // DIAGONALLY TRAVERSE BINARY TREE;
  vector<int> diagonallytraverse(Node* tree)
  {
- // CODE FOR TRAVERSING DIAGONALLY FROM TOP RIGHT FASHION:
+ // CODE FOR TRAVERSING DIAGONALLY FROM TOP RIGHT FASHION:  SIMILAR TO POST ORDER (right to left) but we code if(curr->left) 
      vector<int>ans;
      queue<Node*>q;
      q.push(root);
@@ -22,7 +22,24 @@ using namespace std;
 
  }
 
-
+vector<int> diagonllytraverse(Node* tree)
+{
+ // CODE FOR TRAVERSING DIAGONALLY FROM TOP LEFT FASHION:  SIMILAR TO PRE ORDER (left to right) but we code if(curr->right)
+ vector<int>ans;
+ queue<Node*>q;
+ q.push(root);
+ while(!q.empty()){
+  Node* curr=q.front();
+  q.pop();
+  while(curr){
+   if(curr->right) q.push(curr->left);
+   ans.push_back(curr->data);
+   curr=curr->right;
+  }
+ }
+ return ans;
+}
+   
 
 
 
