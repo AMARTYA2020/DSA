f0bc73106713abf78b865c27f58e91e17cb3ea44
Amartya pandey <amartyapandey19@gmail.com>
2021-02-10 09:04:34 +0530

    Add files via upload

---
+++ Copy List with Random Pointers.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// COPY LIST WITH RANDOM POINTER:
+class solution{
+public:
+    Node* copyRandomList(Node* head){
+        Node* curr=head;
+        while(curr!=NULL){
+            Node* next=curr->next;
+            curr->next=new Node(curr->val);
+            curr->next->next=next;
+            curr=next;
+        }
+        curr=head;
+        while(curr!=NULL){
+            if(curr->random!=NULL){
+                curr->next->random=curr->random->next;
+            }else{
+                curr->next->random=NULL;
+
+            }
+            curr=curr->next->next;
+        }
+        Node* newhead=new Node(0);
+        Node* copycurr=newhead;
+        curr=head;
+        while(curr!=NULL){
+            copycurr->next=curr->next;
+            curr->next=copycurr->next->next;
+            curr=curr->next;
+            copycurr=copycurr->next;
+        }
+        return newnode->next;
+    }
+};
