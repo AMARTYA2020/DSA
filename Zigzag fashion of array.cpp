d13366ad34e77945a1465032a8688df8c1022cfc
Amartya pandey <amartyapandey19@gmail.com>
2021-02-14 09:58:27 +0530

    Update Zigzag fashion of array.cpp

--- Zigzag fashion of array.cpp
+++ Zigzag fashion of array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void zigzagfashion(int arr[],int n)
 {
     for(int i=0;i<n;i++){
-        if(i%2==0){
+        if(i%2==0){             // AT EVERY ODD PLACE WE SEE THAT THE ELEMENT IS GREATER THEN BOTH THE SIDES
             if(arr[i]>arr[i+1]){
                 swap(arr[i],arr[i+1]);
             }
