cd3a833898ddbb228db5cdc85c9e1192c9f4b487
Amartya pandey <amartyapandey19@gmail.com>
2021-02-16 08:33:26 +0530

    Update Implement Atoi.cpp

--- Implement Atoi.cpp
+++ Implement Atoi.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// String to integer
 int atoi(string s)
 {
     int result=0;
@@ -15,7 +15,7 @@ int atoi(string s)
         ++i;
     }
     while(s[i]){
-        if(s[i]-'0'>=0 && s[i]-'0'<=9)
+        if(s[i]-'0'>=0 && s[i]-'0'<=9)    
             result=result*10+(s[i]-'0');
         else
             return -1;
