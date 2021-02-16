509646dc25018a34dde74913fe4b7aac80508643
Amartya pandey <amartyapandey19@gmail.com>
2021-02-16 08:32:45 +0530

    Add files via upload

---
+++ Implement Atoi.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int atoi(string s)
+{
+    int result=0;
+    int sign=1;
+    int i=0;
+    int n=s.length();
+    if(s[i]=='-'){
+        sign=-1;
+        ++i;
+    }
+    if(s[i]==' ' && i<n){
+        ++i;
+    }
+    while(s[i]){
+        if(s[i]-'0'>=0 && s[i]-'0'<=9)
+            result=result*10+(s[i]-'0');
+        else
+            return -1;
+    ++i;
+    }
+    return result*sign
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
