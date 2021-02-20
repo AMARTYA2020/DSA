a80c43e2d188531142c1d72020fe08b0b7ce3ff8
Amartya pandey <amartyapandey19@gmail.com>
2021-02-20 11:31:50 +0530

    Update Check if a Binary Tree is a BST.cpp

--- Check if a Binary Tree is a BST.cpp
+++ Check if a Binary Tree is a BST.cpp
@@ -15,7 +15,7 @@ bool isBST(Node* root)
     vector<int>v;
     inorder(root,v);
     for(int i=0;i<v.size()-1;i++){
-        if(v[i]>=v[i+1]){
+        if(v[i]>=v[i+1]){            // AS INORDER TRAVERSAL IS SORTED IN ASCENDING ORDER: if in decending order return false
             return false;
         }
     }
